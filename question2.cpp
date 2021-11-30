0f1e12c4ecfc6a4954aa3e890a635a9e0f9e5a7f
Shruti Grover <shruti.grover2001@gmail.com>
2021-11-30 15:45:26 +0530

    done

---
+++ question2.cpp
@@ -0,0 +1,46 @@
+//Sort the activities according to their finishing time 
+// Select the first activity from the sorted array and print it. 
+// Do the following for the remaining activities in the sorted array. 
+// If the start time of this activity is greater than or equal to the finish time of the previously selected activity then select this activity and print it.
+// // Prints a maximum set of activities that can be done by a single
+// person, one at a time.
+//  n   -->  Total number of activities
+//  s[] -->  An array that contains start time of all activities
+//  f[] -->  An array that contains finish time of all activities
+
+
+#include <bits/stdc++.h>
+using namespace std;
+  
+
+void printMaxActivities(int s[], int f[], int n)
+{
+    int i, j;
+  
+    cout <<"Following activities are selected "<< endl;
+  
+    
+    i = 0;
+    cout <<" "<< i;
+  
+    
+    for (j = 1; j < n; j++)
+    {
+      
+      if (s[j] >= f[i])
+      {
+          cout <<" " << j;
+          i = j;
+      }
+    }
+}
+  
+
+int main()
+{
+    int s[] =  {1, 3, 0, 5, 8, 5};
+    int f[] =  {2, 4, 6, 7, 9, 9};
+    int n = sizeof(s)/sizeof(s[0]);
+    printMaxActivities(s, f, n);
+    return 0;
+}
